d2c53677779490fb8c15158111842ba657d0a005
ToeCrow <thomas.kronvold@chasacademy.se>
2025-03-22 20:08:48 +0100

    all pages responsive with multiple columns

--- src/pages/ProfileForm.jsx
+++ src/pages/ProfileForm.jsx
@@ -120,148 +120,154 @@ const ProfileForm = () => {
 
   return (
     <main className="flex flex-col items-center p-4 gap-4">
-      <form className="w-full bg-white p-6 rounded-lg shadow-md mt-4 fixed-width">
-        <h2 className="text-2xl font-bold text-center text-green-600 mb-4">
-          Profil
-        </h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 items-start p-4 max-w-4xl w-full'>
+        <div className="flex flex-col gap-4">
+          <form className="w-full bg-white p-6 rounded-lg shadow-md mt-4 fixed-width">
+            <h2 className="text-2xl font-bold text-center text-green-600 mb-4">
+              Profil
+            </h2>
 
-        <div className="mb-4">
-          <fieldset className="flex flex-col">
-            <legend className="label-custom">
-              Kön <span className="font-normal">(viktig för uträkning av kalorier)</span>
-              :
-            </legend>
-            <div className="flex">
-              <label className="block m-4">
-                <input
-                  type="radio"
-                  id="gender-male"
-                  name="gender"
-                  value="male"
-                  checked={gender === "male"}
-                  onChange={() => dispatch(setGender("male"))}
-                  required
-                  className="mr-2"
-                />
-                Man
-              </label>
-              <label className="block m-4">
-                <input
-                  type="radio"
-                  id="gender-female"
-                  name="gender"
-                  value="female"
-                  checked={gender === "female"}
-                  onChange={() => dispatch(setGender("female"))}
-                  required
-                  className="mr-2"
-                />
-                Kvinna
-              </label>
+            <div className="mb-4">
+              <fieldset className="flex flex-col">
+                <legend className="label-custom">
+                  Kön <span className="font-normal">(viktig för uträkning av kalorier)</span>
+                  :
+                </legend>
+                <div className="flex">
+                  <label className="block m-4">
+                    <input
+                      type="radio"
+                      id="gender-male"
+                      name="gender"
+                      value="male"
+                      checked={gender === "male"}
+                      onChange={() => dispatch(setGender("male"))}
+                      required
+                      className="mr-2"
+                    />
+                    Man
+                  </label>
+                  <label className="block m-4">
+                    <input
+                      type="radio"
+                      id="gender-female"
+                      name="gender"
+                      value="female"
+                      checked={gender === "female"}
+                      onChange={() => dispatch(setGender("female"))}
+                      required
+                      className="mr-2"
+                    />
+                    Kvinna
+                  </label>
+                </div>
+              </fieldset>
             </div>
-          </fieldset>
+
+            <label htmlFor="weight" className="label-custom">
+              Vikt (kg):
+            </label>
+            <input
+              type="number"
+              id="weight"
+              name="weight"
+              value={weightInput || ""}
+              onChange={handleWeightChange}
+              required
+              className={`block w-full border rounded p-2 mb-4 ${weightInput ? "" : "border-accent border-2"}`}
+            />
+
+            <label htmlFor="height" className="label-custom">
+              Längd (cm):
+            </label>
+            <input
+              type="number"
+              id="height"
+              name="height"
+              value={height || ""}
+              onChange={(e) => dispatch(setHeight(e.target.value))}
+              required
+              className={`block w-full border rounded p-2 mb-4 ${height ? "" : "border-accent border-2"}`}
+            />
+
+            <label htmlFor="birthdate" className="label-custom">
+              Födelsedatum (yyyy-mm-dd):
+            </label>
+            <input
+              type="date"
+              id="birthdate"
+              name="birthdate"
+              value={birthDate || ""}
+              onChange={handleBirthDateChange}
+              required
+              className={`block w-full border rounded p-2 mb-4 ${birthDate ? "" : "border-accent border-2"}`}
+            />
+
+            <label htmlFor="activity-level" className="label-custom">
+              Aktivitetsnivå:
+            </label>
+            <select
+              id="activity-level"
+              value={activityLevel}
+              required
+              onChange={(e) =>
+                dispatch(setActivityLevel(parseFloat(e.target.value)))
+              }
+              className={`block w-full border rounded p-2 mb-4 ${activityLevel ? "" : "border-accent border-2"}`}
+            >
+              <option value={1.2}>Stillasittande (lite eller ingen träning)</option>
+              <option value={1.375}>
+                Lätt aktiv (lätt träning/sport 1-3 dagar/vecka)
+              </option>
+              <option value={1.55}>
+                Måttligt aktiv (måttlig träning/sport 3-5 dagar/vecka)
+              </option>
+              <option value={1.725}>
+                Mycket aktiv (hård träning/sport 6-7 dagar/vecka)
+              </option>
+              <option value={1.9}>
+                Elit/idrottare (mycket hård träning, fysiskt jobb)
+              </option>
+            </select>
+
+            <label htmlFor="goal" className="label-custom">
+              Mål:
+            </label>
+            <select
+              id="goal"
+              value={goal}
+              onChange={(e) => dispatch(setGoal(parseFloat(e.target.value)))}
+              className={`block w-full border rounded p-2 mb-4 `}
+            >
+              <option value="-500">Gå ner i vikt</option>
+              <option value="0">Hålla vikten</option>
+              <option value="500">Gå upp i vikt</option>
+            </select>
+
+            <Button
+              onClick={handleSave}
+              className="w-full"
+            >
+              Spara
+            </Button>
+            <p
+              className={`p-2 rounded m-2 ${errorMessage ? "border-2 border-accent" : ""}`}
+            >
+              {errorMessage}
+            </p>
+
+            {tdee !==0 && (
+              <h2 className="text-lg text-green-600 mt-4 text-center">
+                Ditt totala dagliga energibehov: <br />
+                <span className="font-bold">{Math.round(tdee)} kcal</span>
+              </h2>
+            )}
+          </form>
         </div>
-
-        <label htmlFor="weight" className="label-custom">
-          Vikt (kg):
-        </label>
-        <input
-          type="number"
-          id="weight"
-          name="weight"
-          value={weightInput || ""}
-          onChange={handleWeightChange}
-          required
-          className={`block w-full border rounded p-2 mb-4 ${weightInput ? "" : "border-accent border-2"}`}
-        />
-
-        <label htmlFor="height" className="label-custom">
-          Längd (cm):
-        </label>
-        <input
-          type="number"
-          id="height"
-          name="height"
-          value={height || ""}
-          onChange={(e) => dispatch(setHeight(e.target.value))}
-          required
-          className={`block w-full border rounded p-2 mb-4 ${height ? "" : "border-accent border-2"}`}
-        />
-
-        <label htmlFor="birthdate" className="label-custom">
-          Födelsedatum (yyyy-mm-dd):
-        </label>
-        <input
-          type="date"
-          id="birthdate"
-          name="birthdate"
-          value={birthDate || ""}
-          onChange={handleBirthDateChange}
-          required
-          className={`block w-full border rounded p-2 mb-4 ${birthDate ? "" : "border-accent border-2"}`}
-        />
-
-        <label htmlFor="activity-level" className="label-custom">
-          Aktivitetsnivå:
-        </label>
-        <select
-          id="activity-level"
-          value={activityLevel}
-          required
-          onChange={(e) =>
-            dispatch(setActivityLevel(parseFloat(e.target.value)))
-          }
-          className={`block w-full border rounded p-2 mb-4 ${activityLevel ? "" : "border-accent border-2"}`}
-        >
-          <option value={1.2}>Stillasittande (lite eller ingen träning)</option>
-          <option value={1.375}>
-            Lätt aktiv (lätt träning/sport 1-3 dagar/vecka)
-          </option>
-          <option value={1.55}>
-            Måttligt aktiv (måttlig träning/sport 3-5 dagar/vecka)
-          </option>
-          <option value={1.725}>
-            Mycket aktiv (hård träning/sport 6-7 dagar/vecka)
-          </option>
-          <option value={1.9}>
-            Elit/idrottare (mycket hård träning, fysiskt jobb)
-          </option>
-        </select>
-
-        <label htmlFor="goal" className="label-custom">
-          Mål:
-        </label>
-        <select
-          id="goal"
-          value={goal}
-          onChange={(e) => dispatch(setGoal(parseFloat(e.target.value)))}
-          className={`block w-full border rounded p-2 mb-4 `}
-        >
-          <option value="-500">Gå ner i vikt</option>
-          <option value="0">Hålla vikten</option>
-          <option value="500">Gå upp i vikt</option>
-        </select>
-
-        <Button
-          onClick={handleSave}
-          className="w-full"
-        >
-          Spara
-        </Button>
-        <p
-          className={`p-2 rounded m-2 ${errorMessage ? "border-2 border-accent" : ""}`}
-        >
-          {errorMessage}
-        </p>
-
-        {tdee !==0 && (
-          <h2 className="text-lg text-green-600 mt-4 text-center">
-            Ditt totala dagliga energibehov: <br />
-            <span className="font-bold">{Math.round(tdee)} kcal</span>
-          </h2>
-        )}
-      </form>
-      <WeightList weightData={Array.isArray(weight) ? weight : []} />
+        <div className="flex flex-col gap-4">
+          <WeightList weightData={Array.isArray(weight) ? weight : []} />
+        </div>
+      </div>
     </main>
   );
 };
