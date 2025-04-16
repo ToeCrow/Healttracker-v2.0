85392ff90db01628d27ab856bf0059399ec275c7
ToeCrow <thomas.kronvold@chasacademy.se>
2025-04-16 10:59:42 +0200

    updated kcalstatus to work with addmeal

--- src/components/dashboard/KcalStatus.jsx
+++ src/components/dashboard/KcalStatus.jsx
@@ -6,20 +6,28 @@ const KcalStatus = () => {
   const tdee = useSelector(selectTDEE);
   const meals = useSelector((state) => state.meals.meals || []);
 
-  const todaysDate = new Date().toISOString().split('T')[0];
+  const todaysDate = new Date().toLocaleDateString('sv-SE', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
   // Filtrera måltider för idag
   const todaysMeals = meals.filter((meal) => meal.date === todaysDate);
 
   // Räkna ut kalorier som konsumerats idag
-  const consumedCalories = todaysMeals.reduce((total, meal) => total + Number(meal.energy || 0), 0);
+  const consumedCalories = todaysMeals.reduce(
+    (total, meal) => total + Number(meal.total?.kcal || 0), 
+    0
+  );
 
   // Räkna ut om det är kvar eller över
   const remainingKcal = tdee - consumedCalories;
   const isOver = remainingKcal < 0;
 
   return (
-    <div className="flex justify-center items-center max-w-sm mx-auto  shadow-md mt-4 fixed-width bg-card flex-col gap-2 text-card-foreground w-fit rounded-md h-fit drop-shadow-[1px_1px_4px_rgba(0,0,0,0.1)] px-4 pt-6 pb-8">
+    <div className="flex justify-center items-center max-w-sm mx-auto shadow-md mt-4 fixed-width bg-card flex-col gap-2 text-card-foreground w-fit rounded-md h-fit drop-shadow-[1px_1px_4px_rgba(0,0,0,0.1)] px-4 pt-6 pb-8">
       <div 
         className={`w-32 h-32 flex flex-col justify-center items-center rounded-full border-4 
           ${isOver ? 'border-red-500 text-red-500' : 'border-green-500 text-green-500'}`}
